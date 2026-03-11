2621d21b3b582da41a0aee5ef622ac3ab393f952
EvilRadzio <Radzio89k.yt@wp.pl>
2026-03-11 11:58:09 +0100

    level editor update

--- src/Game/Scenes/LevelEditor.hpp
+++ src/Game/Scenes/LevelEditor.hpp
@@ -6,9 +6,12 @@ namespace Scenes
 {
 	class LevelEditor : public Scene
 	{
+	private:
+		px::Map LE_map;
+		int currentTile = 0;
 	public:
-
-		LevelEditor(px::ApiScene api) : Scene(api) {}
+			
+		LevelEditor(px::ApiScene api) : Scene(api), LE_map(sceneApi.tiles.emptyHandle(), 25, 25) {}
 
 		void updateGui(Context& context, px::ApiUpdateGui& api) override;
 		void update(Context& context, px::ApiUpdate& api) override;
