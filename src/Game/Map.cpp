06644f16c84ea28cb87a6ae3ca2106edeb436b28
Barczak Wiktor <wbarczak67@gmail.com>
2026-04-11 14:15:56 +0200

    Added some comments with brief instructions to Map.cpp

--- src/Game/Map.cpp
+++ src/Game/Map.cpp
@@ -8,12 +8,17 @@ namespace nl = nlohmann;
 
 void saveMap(const std::filesystem::path& path, const Map& map)
 {
-	SPDLOG_INFO("Saved the map to {}", path);
-	SPDLOG_ERROR("Failed to save the map to {}", path);
+	// Uzyj tych macro logujacych by ladnie bylo w konsoli widac co sie dzieje
+
+	//SPDLOG_INFO("Saved the map to {}", path);
+	//SPDLOG_ERROR("Failed to save the map to {}", path);
 }
 
 Map loadMap(const std::filesystem::path& path)
 {
-	SPDLOG_INFO("Loaded the map from {}", path);
-	SPDLOG_ERROR("Failed to load the map from {}", path);
+	// Mialo by sens pusta mape w jakis wymiarach dac jesli cos erroruje
+	// bo w grze lepiej by sie pokazala mapa ktora ma napis ze cos poszlo nie tak niz by wywalilo gre
+
+	//SPDLOG_INFO("Loaded the map from {}", path);
+	//SPDLOG_ERROR("Failed to load the map from {}", path);
 }
