51f8e062e40c69b978a4f1c80229f160f83f679f
Barczak Wiktor <wbarczak67@gmail.com>
2026-04-15 16:13:28 +0200

    Apparently I didn't save the file so just pushing everything

--- src/pXL/App/Scene.hpp
+++ src/pXL/App/Scene.hpp
@@ -23,15 +23,14 @@ namespace px
 	struct EngineApi
 	{
 		SceneCommands& comms;
-		const InputRaw& input;
 		const Assets& assets;
-		Transition& transition;
 		const Mapping& mapping;
 	};
 
 	struct SceneCtx
 	{
 		SceneConfig& properties;
+		Transition& transition;
 
 	private:
 
@@ -54,6 +53,31 @@ namespace px
 		float alpha{};
 	};
 
+	// This stuff gotta be replaced with contexts that are above
+
+	struct ApiDraw
+	{
+		sf::RenderTarget& window;
+		const Assets& assets;
+	};
+
+	struct ApiUpdate
+	{
+		const sf::Window& window;
+		const sf::Time dt;
+		Transition& transition;
+	};
+
+	struct ApiScene
+	{
+		SceneCommands& comms;
+		const InputRaw& input;
+		const Assets& assets;
+		SceneConfig& properties;
+		Transition& transition;
+		const Mapping& mapping;
+	};
+
 	class SceneStack;
 
 	class Scene
@@ -71,7 +95,6 @@ namespace px
 	protected:
 
 		ApiScene scene;
-		EngineApi api;
 
 	private:
 
